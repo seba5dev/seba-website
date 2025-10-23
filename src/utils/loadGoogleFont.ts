27c1ff5eddcd92a152b438b118b33759109aebb4
Sebastian Martínez <sebasmartinez05@outlook.com>
2025-10-23 11:27:28 -0500

    Agregar soporte para la fuente "Raleway" en el diseño y actualizar los estilos globales para utilizarla

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -36,14 +36,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Raleway",
+      font: "Raleway",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Raleway",
+      font: "Raleway",
       weight: 700,
       style: "bold",
     },
